8e3c31b0918cf38afff273820d8032ef40a9f7aa
martina-nu <martinanu98@gmail.com>
2022-08-14 01:06:07 +0000

    Add solution

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Template for Machine Learning projects
+# Image Classifier Project
+
+**The dataset is comprised of photos of dogs and cats provided as a subset of photos from a much larger dataset of 3 million manually annotated photos. The dataset was developed as a partnership between Petfinder.com and Microsoft.**
+
+**The dataset was originally used as a CAPTCHA, that is, a task that it is believed a human finds trivial, but cannot be solved by a machine, used on websites to distinguish between human users and bots. The task was referred to as "Asirra". When "Asirra" was presented, it was mentioned 'that user studies indicate it can be solved by humans 99.6% of the time in under 30 seconds. Barring a major advance in machine vision, we expect computers will have no better than a 1/54,000 chance of solving it'.**
+
+**At the time that the competition was posted, the state-of-the-art result was achieved with an SVM and described in a 2007 paper with the title “Machine Learning Attacks Against the Asirra CAPTCHA” (PDF) that achieved 80% classification accuracy. It was this paper that demonstrated that the task was no longer a suitable task for a CAPTCHA soon after the task was proposed.**
+
+**The dataset is straightforward to understand and small enough to fit into memory and get started with computer vision and convolutional neural networks.**
+
+
+Dataset links:
+
+https://www.kaggle.com/c/dogs-vs-cats/data
--- src/app.py
+++ src/app.py
@@ -1,4 +1,89 @@
-from utils import db_connect
-engine = db_connect()
+# Step 0. Load libraries and custom modules
+# System --------------------------------------------------------
+import os
+import pathlib
+# Dataframes and matrices ---------------------------------------
+import numpy as np
+import pandas as pd
+# Graphics ------------------------------------------------------
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+# Machine learning ----------------------------------------------
+from sklearn.model_selection import train_test_split
+# Deep learning -------------------------------------------------
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D,Dense,Dropout, Flatten
+from tensorflow.keras.layers import Activation, BatchNormalization
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras import datasets, layers, models
+from keras.utils import load_img
+from keras.utils import img_to_array
+from keras.utils import get_file
+from keras.utils import image_dataset_from_directory
 
-# your code here
+
+data_dir = pathlib.Path('./Pets')
+
+# Create train dataset
+IMAGE_WITDH = 200
+IMAGE_HEIGHT = 200
+BATCH_SIZE = 32
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(200, 200),
+  batch_size=32)
+
+
+# Create validation dataset
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(200, 200),
+  batch_size=32)
+
+
+# Create the deep learning architecture
+IMAGE_CHANNELS=3
+IMAGE_WIDTH=200
+IMAGE_HEIGHT=200
+model = Sequential([
+
+Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)),
+BatchNormalization(),
+MaxPooling2D(pool_size=(2, 2)),
+Dropout(0.25),
+
+Conv2D(64, (3, 3), activation='relu'),
+BatchNormalization(),
+MaxPooling2D(pool_size=(2, 2)),
+Dropout(0.25),
+
+Conv2D(128, (3, 3), activation='relu'),
+BatchNormalization(),
+MaxPooling2D(pool_size=(2, 2)),
+Dropout(0.25),
+
+Flatten(),
+Dense(512, activation='relu'),
+BatchNormalization(),
+Dropout(0.5),
+Dense(1, activation='sigmoid'), # 2 because we have cat and dog classes
+])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.summary()
+
+# Fit the model
+history=model.fit(train_ds, validation_data=val_ds, epochs=10)
+
+# Save model
+
+model.save('model.h5')
